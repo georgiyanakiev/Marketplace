b1c0207ddce2c70f7b941c158f4c47e8cc8c188b
Georgi Yanakiev <g.yanakiev@yahoo.com>
2020-07-15 19:41:24 +0100

    added delete and update actions with entity framework

---
+++ Marketplace.Web/Controllers/AuctionsController.cs
@@ -0,0 +1,81 @@
+﻿using Marketplace.Entities;
+using Marketplace.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Marketplace.Web.Controllers
+{
+    public class AuctionsController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            AuctionsService service = new AuctionsService();
+
+
+            var auctions = service.GetAllAuctions();
+
+            return View(auctions);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Auction auction)
+        {
+            AuctionsService service = new AuctionsService();
+
+            service.SaveAuction(auction);
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int ID)
+        {
+            AuctionsService service = new AuctionsService();
+
+            var auction = service.GetAuctionByID(ID);
+
+
+            return View(auction);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Auction auction)
+        {
+            AuctionsService service = new AuctionsService();
+
+            service.UpdateAuction(auction);
+
+            return View(auction);
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int ID)
+        {
+            AuctionsService service = new AuctionsService();
+
+            var auction = service.GetAuctionByID(ID);
+
+
+            return View(auction);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Auction auction)
+        {
+            AuctionsService service = new AuctionsService();
+
+            service.DeleteAuction(auction);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
